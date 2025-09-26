5072eec39740bb031dd421702f63983a9ace3634
awvidigal <allanwgvidigal@gmail.com>
2025-09-26 06:34:19 -0300

    preparndo tableau ok

--- simplex.py
+++ simplex.py
@@ -1,7 +1,80 @@
 import numpy as np
 import pandas as pd
 
-MAX_ITERATIONS = 20
+MAX_ITERATIONS  = 200
+BASEIN_COLOR    = 'green!10'
+BASEOUT_COLOR   = 'red!10'
+
+def verificaTablauPreparado(tableau):
+    '''
+    Parametros:
+    -----------
+        tableau:
+            Dataframe que representa o tableau inicial
+
+
+    Return:
+    -------
+        True: se está na forma preparada
+        False: caso contrário
+    '''
+
+    columnsLabels   = tableau.columns
+    rowsLabels      = tableau.index
+    
+    base = rowsLabels.tolist()
+    base.remove('z')
+
+    # isPrepared = True
+
+    for var in base:
+        if tableau.loc['z', var]:
+            return False
+
+    for index in rowsLabels:
+        if index != 'z':
+            if tableau.loc[index, index] != 1:
+                return False
+
+            for var in base:
+                if index != var:
+                    if tableau.loc[index, var]:
+                        return False
+    
+    return True
+
+def preparaTableau(tableau):
+    '''
+    Parametros:
+    -----------
+        tableau:
+            Dataframe que representa o tableau inicial
+
+
+    Return:
+    -------
+        tableau:
+            Dataframe contendo o tableau na forma preparada
+    '''
+    columnsLabels   = tableau.columns
+    rowsLabels      = tableau.index
+    dictColumns     = {rotulo: indice for indice, rotulo in enumerate(columnsLabels)}
+
+    base = rowsLabels.tolist()
+    base.remove('z')
+
+
+    for var in base:
+        if tableau.loc[var,var] > 1:
+            tableau.loc[var] = tableau.loc[var] / tableau.loc[var,var]
+
+        for row in tableau.itertuples():
+            if row.Index != var:
+                if row[dictColumns[var] + 1]:
+                    multValue = -row[dictColumns[var] + 1]
+                    tableau.loc[row.Index] = tableau.loc[row.Index] + (multValue * tableau.loc[var])
+
+    return tableau
 
 
 def simplex(A: list, b: list, c: list, x0: list):
@@ -13,7 +86,7 @@ def simplex(A: list, b: list, c: list, x0: list):
         deve ser a matriz preparada de forma que Ax = b
     
     x0:
-        deve conter os indices das variaveis basicas paraa solução inicial
+        deve conter os indices das variaveis basicas para a solução inicial
     '''
     
     # 1. montar o algoritmo para problemas onde a solução inicial é dada
@@ -23,14 +96,16 @@ def simplex(A: list, b: list, c: list, x0: list):
 
     # Matrizes de teste
     A = np.array([
-        [1,-2,1,0,0],
-        [-2,1,0,1,0],
-        [5,3,0,0,1]    
+        [-2,0,3,1,0,0,0,0],
+        [2,1,2,0,1,0,0,0],
+        [0,-1,3,0,0,1,0,0],
+        [3,3,0,0,0,0,1,0],
+        [1,-1,-3,0,0,0,0,1]    
     ])
 
-    b = np.array([0,4,15])
-    c = np.array([1,3])
-    x0 = np.array([3,4,5])
+    b = np.array([6,7,7,8,9])
+    c = np.array([-2,0,-3])
+    x0 = np.array([4,5,6,7,8])
 
 
     # 1. Montar o tableau utilizando x0 como solução inicial
@@ -47,13 +122,14 @@ def simplex(A: list, b: list, c: list, x0: list):
     b = b.reshape(-1,1)
 
     # 1.3. Montando o tableau
-    tableau = np.vstack([-c, A])
+    tableau = np.vstack([c, A])
     tableau = np.hstack([tableau, b])
 
     # 1.4. Adicionando índices de linhas e colunas
     columns = np.arange(0, aColumns, 1)
     indexCol = ['x' + str(value+1) for value in columns]
     indexCol.append('LD')
+    labelBase = ['x' + str(value+1) for value in x0]
 
     indexRow = ['x' + str(value) for value in x0]
     indexRow.insert(0,'z')
@@ -64,17 +140,13 @@ def simplex(A: list, b: list, c: list, x0: list):
         columns= indexCol
     )
 
+    dictColumns = {rotulo:indice for indice, rotulo in enumerate(tableau.columns)}
+
     # 1.5. Colocando o tableau na forma preparada
-    # para as colunas das variaveis basicas, verificar se são '1' na linha correspondente e '0' nas demais
+    if not verificaTablauPreparado(tableau):
+        tableau = preparaTableau(tableau)
 
-    for row in tableau.itertuples():
-        if row.Index is not 'z':
-            
-        pass
-            
-
-
-
+    # 1.6. Resolvendo o tableau
     for iterations in range(MAX_ITERATIONS):
         # verifica se há valores positivos na linha z do tableau
         notOptimal = (tableau.loc['z'] < 0).any()
@@ -85,6 +157,11 @@ def simplex(A: list, b: list, c: list, x0: list):
             
             # prepara para o teste de bloqueio, considerando apenas valores positivos na coluna
             filterValues = tableau[baseIn] > 0
+            
+            if (filterValues == False).all():
+                print('O problema é ilimitado')
+                break
+            
             tableauCopy = tableau[filterValues].copy()
             
             baseOut = (tableauCopy['LD'] / tableauCopy[baseIn]).idxmin()
@@ -106,6 +183,27 @@ def simplex(A: list, b: list, c: list, x0: list):
                 index= {baseOut: baseIn},
                 inplace= True
             )
+
+            # formatando a saida em latex
+            baseFormat = ['c'] * (len(tableau.columns) + 1)
+            baseFormat[0] = 'l'
+            colorCol = f'>{{\\columncolor{{{BASEIN_COLOR}}}}}c' 
+            baseFormat[indexBaseIn] = colorCol
+            latexFormat = ' '.join(baseFormat)
+
+            latex = tableau.to_latex(
+                column_format= latexFormat,
+                booktabs = False,
+                escape= False
+            )
+
+            rowColor = f'\\rowcolor{{{BASEOUT_COLOR}}}'
+            
+            latex = latex.replace(
+                baseOut,
+                f'\n{rowColor} {baseOut}'
+            )
+
 
 
 
