367e79849d9fd0b4fa0fbd6021dfe2133952b44e
awvidigal <allanwgvidigal@gmail.com>
2025-11-23 17:19:45 -0300

    dijkstra: corrigido contagemd e relaxacoes e montagem da acm

--- acm.py
+++ acm.py
@@ -1,23 +1,29 @@
-import numpy as np
-import entradasGrafos as eg
-from algoritmosGrafos import dijkstra, bellmanFord 
-from entradasGrafos import mapaEUA, redeOtica
+from algoritmosGrafos   import dijkstra, bellmanFord 
+from entradasGrafos     import mapaEUA, redeOtica
 
 saidasRede = [7,14,21]
 saidasMapa = [10, 20, 30, 40, 50, 60, 70]
 
 print ('# ########################## DIJKSTRA #############################')
 
-acm, dp = dijkstra(redeOtica)
+acm, dp, tempo, iteracoes, relaxacoes = dijkstra(redeOtica)
 print('\nRede Otica')
 print('____ _____\n')
+
+print(f'Tempo de execução: {tempo:.6f} segundos')
+print(f'Quantidade de iterações: {iteracoes}')
+print(f'Quantidade de relaxações: {relaxacoes}\n')
 for valor in saidasRede:
     print(f'Nó: {valor}')
     print(f'dist[{valor}]: {dp[valor-1,0]} | prev[{valor}]: {dp[valor-1,1]}\n')
 
-acm, dp = dijkstra(mapaEUA)
+acm, dp, tempo, iteracoes, relaxacoes = dijkstra(mapaEUA)
 print('\nMapa Rodoviario')
 print('____ __________\n')
+
+print(f'Tempo de execução: {tempo:.6f} segundos')
+print(f'Quantidade de iterações: {iteracoes}')
+print(f'Quantidade de relaxações: {relaxacoes}\n')
 for valor in saidasMapa:
     print(f'Nó: {valor}')
     print(f'dist[{valor}]: {dp[valor-1,0]} | prev[{valor}]: {dp[valor-1,1]}\n')
@@ -25,16 +31,24 @@ for valor in saidasMapa:
 
 print ('# ########################## BELLMAN-FORD #############################')
 
-acm, dp = bellmanFord(redeOtica)
+acm, dp, tempo, iteracoes, relaxacoes = bellmanFord(redeOtica)
 print('\nRede Otica')
 print('____ _____\n')
+
+print(f'Tempo de execução: {tempo:.6f} segundos')
+print(f'Quantidade de iterações: {iteracoes}')
+print(f'Quantidade de relaxações: {relaxacoes}\n')
 for valor in saidasRede:
     print(f'Nó: {valor}')
     print(f'dist[{valor}]: {dp[valor-1,0]} | prev[{valor}]: {dp[valor-1,1]}\n')
 
-acm, dp = dijkstra(mapaEUA)
+acm, dp, tempo, iteracoes, relaxacoes = dijkstra(mapaEUA)
 print('\nMapa Rodoviario')
 print('____ __________\n')
+
+print(f'Tempo de execução: {tempo:.6f} segundos')
+print(f'Quantidade de iterações: {iteracoes}')
+print(f'Quantidade de relaxações: {relaxacoes}\n')
 for valor in saidasMapa:
     print(f'Nó: {valor}')
     print(f'dist[{valor}]: {dp[valor-1,0]} | prev[{valor}]: {dp[valor-1,1]}\n')
--- algoritmosGrafos.py
+++ algoritmosGrafos.py
@@ -1,4 +1,5 @@
 import numpy as np
+import time
 
 INI_DIST = 10_000
 DIST = 0
@@ -20,11 +21,26 @@ def dijkstra(mAdjacencia, origem = 0):
     -------
     acm:
         Matriz de adjacência da árvore de caminho mínimo
+
+    dp:
+        Vetor final dist|prev para cada nó
+
+    tempoDeExecucao:
+        Tempo computacional para execução da arvore de caminhos mínimos
+
+    iteracoes:
+        Quantidade de iterações até a árvore
+
+    relaxacoes:
+        Quantidade de relaxacoes até a árvore
     '''
 
     # 1. Inicialização
+    inicio = time.perf_counter()
     adjacencia = np.array(mAdjacencia)
     qtdVertices = adjacencia.shape[0]
+    iteracoes = 0
+    relaxacoes = 0
 
     # 1.1. criar vetor dist | prev
     dp = np.zeros((qtdVertices,2))
@@ -44,6 +60,7 @@ def dijkstra(mAdjacencia, origem = 0):
     # 2. Inicio da busca pela arvore de caminhos minimos
     while not np.all(vertices):
         # 2.1. identificar os vertices que fazem parte da franja
+        iteracoes += 1
         franja = dp[~vertices]
         indicesFranja = np.where(~vertices)[0]
 
@@ -53,34 +70,24 @@ def dijkstra(mAdjacencia, origem = 0):
 
         # 2.3. inserir o vertice na arvore
         vertices[menorDist] = True
-        acm[menorDist,:] = adjacencia[menorDist,:]
-        
-        # 2.4. buscar as colunas que possuem mais de um valor
-        for indiceColuna, valor in enumerate(acm[menorDist,:]):
-            mascaraColuna = acm[:,indiceColuna] != 0
-            distNova = valor + dp[menorDist,DIST]
+        # acm[menorDist,:] = adjacencia[menorDist,:]
 
-            if np.sum(mascaraColuna) > 1:
-                distAtual = dp[indiceColuna,DIST]                
+        # 2.4. Olhar para a linha correspondente na matriz de adjacencias e efetuar as relaxações
+        for indice, valor in enumerate(adjacencia[menorDist,:]):
+            if valor:
+                distNova = valor + dp[menorDist,DIST]
+                distAtual = dp[indice,DIST]
 
                 if distNova < distAtual:
-                    acm[:,indiceColuna] = 0
-                    acm[menorDist,indiceColuna] = valor
+                    relaxacoes += 1
+                    dp[indice,:] = distNova, menorDist+1
 
-                    dp[indiceColuna,:] = distNova, menorDist+1
-
-                else:
-                    mascaraColuna[menorDist] = False
-                    melhorPrev = np.where(mascaraColuna)[0]
-                    melhorDist = acm[melhorPrev,indiceColuna]
-                    acm[:,indiceColuna] = 0
-                    acm[melhorPrev,indiceColuna] = melhorDist
-                    
-
-            elif valor:
-                dp[indiceColuna,:] = distNova, menorDist+1
-
-    return acm, dp
+    for indice, valor in enumerate(dp):
+        acm[int(valor[1]),indice] = adjacencia[int(valor[1]),indice]
+        
+    final = time.perf_counter()
+    tempoDeExecucao = final - inicio
+    return acm, dp, tempoDeExecucao, iteracoes, relaxacoes
 
 def bellmanFord(mAdjacencia, ordem = 0):
     '''
@@ -98,10 +105,25 @@ def bellmanFord(mAdjacencia, ordem = 0):
     -------
     acm:
         Matriz de adjacência da árvore de caminho mínimo
+
+    dp:
+        Vetor final dist|prev para cada nó
+
+    tempoDeExecucao:
+        Tempo computacional para execução da arvore de caminhos mínimos
+
+    iteracoes:
+        Quantidade de iterações até a árvore
+
+    relaxacoes:
+        Quantidade de relaxacoes até a árvore
     '''
 
     # 1. Inicialização
+    inicio = time.perf_counter()
     adjacencia = np.array(mAdjacencia)
+    iteracoes = 0
+    relaxacoes = 0
 
     # 1.1. criar vetor dist | prev
     dp = np.zeros((adjacencia.shape[0],2))
@@ -121,7 +143,8 @@ def bellmanFord(mAdjacencia, ordem = 0):
             contador += 1
             if contador == 2:
                 break
-
+        
+        iteracoes += 1
         alteraDp = False
         for indice, valor in np.ndenumerate(adjacencia):
             
@@ -130,6 +153,7 @@ def bellmanFord(mAdjacencia, ordem = 0):
                 atualDist = dp[indice[1],DIST]
                 
                 if novaDist < atualDist:
+                    relaxacoes += 1
                     dp[indice[1],:] = novaDist, indice[0]+1
                     alteraDp = True
                     contador = 0
@@ -137,4 +161,6 @@ def bellmanFord(mAdjacencia, ordem = 0):
     for indice, valor in enumerate(dp):
         acm[int(valor[1]),indice] = adjacencia[int(valor[1]),indice]
 
-    return acm, dp
+    final = time.perf_counter()
+    tempoDeExecucao = final - inicio
+    return acm, dp, tempoDeExecucao, iteracoes, relaxacoes
